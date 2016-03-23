0169754622c14021a63ecf18929a097ea260a06c
Benjamin Spalding <benjamin.spalding02@gmail.com>
2016-03-23 15:56:02 -0400

    Added Files
    This is what I added

---
+++ Homework8/DAL/PetStore.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using Homework8.Models;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace Homework8.DAL
+{
+    public class PetStoreContext : DbContext
+    {
+        public PetsStoreContext() : base("PetsStoreContext")
+    {
+
+    }
+
+    }
+}
